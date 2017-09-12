5681b8308a7f905823096fcaaa7727206f1ca92b
kailaash <kailaash@outlook.com>
2017-09-12 18:35:50 +0200

    Initial Commit

---
+++ index.js
@@ -0,0 +1,26 @@
+var axios = require('axios');
+
+const ROOT_URL = 'https://www.googleapis.com/youtube/v3/search';
+
+module.exports = function (options, callback) {
+  if (!options.key) {
+    throw new Error('Youtube Search expected key, received undefined');
+  }
+
+  var params = {
+    part: 'snippet',
+    key: options.key,
+    q: options.term,
+    type: 'video',
+    maxResults: '50',
+    order: 'relevance'
+  };
+
+  axios.get(ROOT_URL, { params: params })
+    .then(function(response) {
+      if (callback) { callback(response.data.items); }
+    })
+    .catch(function(error) {
+      console.error(error);
+    });
+};
